a3235c89c24f5ec8e8577db8f31c54974f913180
Shravan A <shravan.kgl@gmail.com>
2022-08-02 02:55:33 +0530

    Update game.clj

--- clojure-2408/src/clojure_2408/game.clj
+++ clojure-2408/src/clojure_2408/game.clj
@@ -6,8 +6,7 @@
   (def total-cells (* width width))
   (def win-num win-num)
   (def game-board
-    (let [zero-board (vec (repeat (* total-cells) 0))]
-      (println zero-board)
+    (let [zero-board (vec (repeat (* total-cells) 0))] 
       (add-cell zero-board))))
 
 (defn add-cell [game-board]
@@ -29,7 +28,7 @@
        (filter #(= (second %) 0)
                (map-indexed vector game-board))))
 
-(defn draw-board [game-board width]
+(defn draw-board [game-board]
   "draw the board"
   (doseq [row  (get-rows game-board)] (println row)))
 
@@ -37,7 +36,8 @@
 (defn get-rows [game-board]
   (partition-all 4 game-board))
 
-(defn get-columns [game-board width]
+
+(defn get-columns [game-board]
   (list
    (vec (take-nth 4 game-board))
    (vec (take-nth 4 (drop 1 game-board)))
@@ -46,25 +46,79 @@
 
 
 (defn reverse-rows-or-columns [rows-or-columns]
-  (map reverse rows-or-columns)
-  )
+  (map reverse rows-or-columns))
 
-(defn move-cells [arr width] 
+(defn move-cells [arr]
   (let [non-zero (filter pos? arr)]
-  (let [add-similar (reduce (fn [a b] (if (= a b) [(+ a b) 0] [a b])) non-zero)]
- 
-    (concat non-zero (repeat (- width (count non-zero)) 0))
-    ))
+    (let [add-similar (add-pairs non-zero)]
+      (concat add-similar (repeat (- width (count add-similar)) 0)))))
+
+(defn add-pairs [arr]
+  (loop [r arr
+         output []]
+    (if (empty? r) output
+        (let [cur (first r)
+              next (second r)]
+          (if (= cur next)
+            (recur (drop 2 r) (conj output (* 2 cur)))
+            (recur (drop 1 r) (conj output cur)))))))
+
+(defn move-left [game-board]
+  (mapcat move-cells 
+          (get-rows game-board))
   )
 
-(init-game 4 2408)
+(defn move-right [game-board]
+  (mapcat reverse 
+          (map move-cells 
+               (map reverse 
+                    (get-rows game-board))))
+  )
 
-(draw-board game-board width)
+(defn move-up [game-board]
+  (flatten (get-columns 
+            (mapcat move-cells 
+                    (get-columns game-board))))
+)
 
-game-board
+(defn move-down [game-board]
+  (flatten (get-columns 
+            (mapcat reverse 
+                    (map move-cells 
+                         (map reverse 
+                              (get-columns game-board))))))
+  )
 
-(get-columns game-board width)
-
-(reverse-rows-or-columns (get-rows game-board))
 
 
+(defn play-next [game-board]
+  (println "Press \n w : move up \n a : move left \n s : move down \n d : move right ")
+  (let [input (read-line)]
+    (if (contains? #{"w" "a" "s" "d" "q"} input) 
+      (execute input game-board))))
+
+
+(defn executecase [input game-board]
+  (case input
+    "w" (move-up game-board)
+    "a" (move-left game-board)
+    "s" (move-down game-board)
+    "d" (move-right game-board) 
+    nil
+  ))
+
+(defn execute [input game-board]
+  (cond
+    (= "w" input) (move-up game-board)
+    (= "a" input) (move-left game-board)
+    (= "s" input) (move-down game-board)
+    (= "d" input) (move-right game-board)
+    (= "q" input) (println "quit")
+    ))
+
+(loop [game-board (play-next game-board)]
+  (if (= game-board nil) (println "quit")
+      (do
+      (draw-board game-board)
+      (recur (play-next game-board)))
+  ))
