cedd665e09d0feae9d5f1b342310315eb6b1a18d
Shravan A <shravan.kgl@gmail.com>
2022-08-04 06:17:24 +0530

    Update game.clj

--- clojure-2408/src/clojure_2408/game.clj
+++ clojure-2408/src/clojure_2408/game.clj
@@ -1,27 +1,30 @@
 (ns clojure-2408.game)
 
-(defn get-zero-indicies 
+(defn get-zero-indicies
   "get list of all indicies containing value 0"
   [game-board]
   (map first
        (filter #(= (second %) 0)
                (map-indexed vector game-board))))
 
-(defn pick-random-empty-cell 
+(defn pick-random-empty-cell
   "get a random 0 value cell"
-  [game-board] 
-  (rand-nth (get-zero-indicies game-board)))
+  [game-board]
+  (if (= (apply min game-board) 0)
+  (rand-nth (get-zero-indicies game-board))
+    -1))
 
-(defn pick-rand-value 
+(defn pick-rand-value
   "pick a value between 2(75% probability) and 4(25% probability) "
   []
   (rand-nth [2 2 2 4]))
 
 (defn add-cell
   "add a random value in random 0 value cell"
-   [game-board]
+  [game-board]
   (let [random-cell (pick-random-empty-cell game-board)]
-    (assoc game-board random-cell (pick-rand-value))))
+    (if (= random-cell -1) game-board
+    (assoc (vec game-board) random-cell (pick-rand-value)))))
 
 (defn get-rows [game-board]
   (partition-all 4 game-board))
@@ -34,9 +37,10 @@
    (vec (take-nth 4 (drop 2 game-board)))
    (vec (take-nth 4 (drop 3 game-board)))))
 
-(defn draw-board 
+(defn draw-board
   "draw the board"
   [game-board]
+  (println "")
   (doseq [row  (get-rows game-board)] (println row))
   game-board)
 
@@ -52,43 +56,38 @@
 
 
 (defn move-cells [arr]
-  (let [non-zero (filter pos? arr) width (Math/sqrt (count arr) )]
+  (let [non-zero (filter pos? arr) width (count arr)]
     (let [add-similar (add-pairs non-zero)]
       (concat add-similar (repeat (- width (count add-similar)) 0)))))
 
 (defn move-left [game-board]
-  (mapcat move-cells 
-          (get-rows game-board))
-  )
+  (mapcat move-cells
+          (get-rows game-board)))
 
 (defn move-right [game-board]
-  (mapcat reverse 
-          (map move-cells 
-               (map reverse 
-                    (get-rows game-board))))
-  )
+  (mapcat reverse
+          (map move-cells
+               (map reverse
+                    (get-rows game-board)))))
 
 (defn move-up [game-board]
-  (flatten (get-columns 
-            (mapcat move-cells 
-                    (get-columns game-board))))
-)
+  (flatten (get-columns
+            (mapcat move-cells
+                    (get-columns game-board)))))
 
 (defn move-down [game-board]
-  (flatten (get-columns 
-            (mapcat reverse 
-                    (map move-cells 
-                         (map reverse 
-                              (get-columns game-board))))))
-  )
+  (flatten (get-columns
+            (mapcat reverse
+                    (map move-cells
+                         (map reverse
+                              (get-columns game-board)))))))
 
 (defn execute [input game-board]
   (cond
     (= "w" input) (move-up game-board)
     (= "a" input) (move-left game-board)
     (= "s" input) (move-down game-board)
-    (= "d" input) (move-right game-board)
-    ))
+    (= "d" input) (move-right game-board)))
 
 (defn executecase [input game-board]
   (case input
@@ -98,13 +97,12 @@
     "d" (move-right game-board)
     game-board))
 
-(defn play-next [game-board] 
+(defn play-next [game-board]
   (let [input (read-line)]
-    (if (contains? #{"w" "a" "s" "d" "q"} input) 
-      (if(= input "q") input 
-      (draw-board (add-cell (execute input game-board))))
-      (println "invalid input")
-    )))
+    (if (contains? #{"w" "a" "s" "d" "q"} input)
+      (if (= input "q") input
+          (draw-board (add-cell (execute input game-board))))
+      (do (println "invalid input") game-board))))
 
 (defn play-next-test [game-board]
   (let [input (read-line)]
@@ -116,17 +114,21 @@
 (defn check-win [game-board win-num]
   (cond
     (contains? game-board win-num)  (println "you win")
-    (= (contains? game-board 0) false) (println "you lose")))
+    (= (contains? game-board 0) false) (println "you lose")
+    :else game-board))
 
 
-(defn play-2048 
+(defn play-2048
   "Play 2048 the game"
-  [width win-num] 
+  [width win-num]
   (println "Press \n w : move up \n a : move left \n s : move down \n d : move right \n q : quit")
   (let [total-cells (* width width)
         game-board (add-cell (vec (repeat (* total-cells) 0)))]
-    (draw-board game-board) 
+    (draw-board game-board)
     (loop [input game-board]
       (if (= input "q") (println "game over")
-          (recur (play-next input)))))
-    ) 
+          (cond
+            (= (apply max input) win-num)  (println "you win")
+            (> (apply min input) 0)  (println "you lose")
+            :else (recur (play-next input)))))))
+
